812ee00544e7aa12ddebbb5b81a9ae4203667567
paco92 <43986096+paco92@users.noreply.github.com>
2018-12-04 10:37:39 +0100

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>  
+    <h1>Clicked {{ number }} time(s)</h1>
+    <button @click="iAmClicked()">Click me!</button>
+  </div>
+</template>
+ 
+<script>
+    export default {
+        data() {
+            return {
+            title: 'Not clicked yet',
+            number: 0
+            }
+        },
+        methods: {
+            iAmClicked() {
+                this.number++;
+            }
+        }
+    };
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
